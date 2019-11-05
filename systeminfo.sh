db81d857b24037dfe3aa6e0eea86c634878c863c
Jack <jackclark340@gmail.com>
2019-11-05 16:36:56 +0000

    Check if running with sudo
    Added check_sudo_permission to validate whether the script is being run with sudo permissions.
    
    Script must be run with sudo as it is required for performing some tasks.
    
    Signed-off-by: Jack <jackclark340@gmail.com>

--- systeminfo.sh
+++ systeminfo.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 ERROR=$(echo -en '\033[01;31m')
 PASS=$(echo -en '\033[01;32m')
 NC=$(echo -en '\033[0m')
+#WARN=$(echo -en '\033[1;33m')
 INFO=$(echo -en '\033[01;35m')
 
 function usage {
@@ -17,3 +18,25 @@ Usage:
 EOF
 		exit 0
 }
+
+#Check if sudo 
+function check_sudo_permission {
+	echo "${INFO}[*]${NC} Checking if sudo user...${NC}"
+
+	if [ "$EUID" -ne 0 ]; then
+		echo "${ERROR}[-]${NC} Sudo perimissions are required. Please run again with sudo..."
+		return 1
+	else
+		
+		echo "${PASS}[+]${NC} Running as sudo...${NC}"
+		return 0
+	fi 
+}
+
+function main {
+
+	check_sudo_permission
+	
+}
+
+main "$@"
