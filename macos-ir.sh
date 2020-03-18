d14018e2d684bba14a21abf69e078ae11735c896
Jack <jackclark340@gmail.com>
2020-03-18 20:57:33 +0000

    Use a main file to call anlaysis or collect.
    Added a new file, macos-ir.sh, which will act as the initial
    entry-point to the scripts. At this time it is empty, however, it is to
    act as a replacement script which will accept arguments similar to
    analysis.sh and collect.sh, and will also install the tools. This
    doesn't need to be run with sudo, which fixes an issue that I was
    having with installing the analysis tools in the original script (the
    script needed to be run with sudo, but Homebrew won't install if
    executed using sudo).

---
+++ macos-ir.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+# -e exit if any command returns non-zero status code
+# -u prevent using undefined variables
+# -o pipefail force pipelines to fail on first non-zero status code
+
+FAIL=$(echo -en '\033[01;31m')
+PASS=$(echo -en '\033[01;32m')
+NC=$(echo -en '\033[0m')
+INFO=$(echo -en '\033[01;35m')
+WARN=$(echo -en '\033[1;33m')
+
+IFS=$'\n'
+
+function main {
+	true
+}
+
+main "$@"
